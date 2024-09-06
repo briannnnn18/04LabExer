b9efd630f05457b9b4ad49633c5ef1191300b7c5
ACER <ACER@DESKTOP-N870L3J>
2024-09-06 21:50:55 +0800

    Add project files.

---
+++ 04-Laboratory-Exercise-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ApplePurchase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of apples you want to purchase: ");
+            int numApples = Convert.ToInt32(Console.ReadLine());
+
+            double ApplePrice= 32.50;
+            double TPrice = numApples * ApplePrice;
+
+            Console.WriteLine($"The total price of {numApples} apples is {TPrice:F2}.");
+
+            int convertedPrice = (int)TPrice;
+            Console.WriteLine($"The value of the converted price is {convertedPrice}.");
+
+            Console.ReadLine();
+        }
+    }
+}
